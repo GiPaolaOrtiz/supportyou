fc2588bcee9d6c7471d7bedfdf60f3dea5088dc1
JhnFbre <walterfa@outlook.es>
2017-07-09 21:41:14 -0500

    Updating nr 2

--- js/pago.js
+++ js/pago.js
@@ -1,3 +1,14 @@
+document.getElementById("paypal").checked=false;
+document.getElementById("other").checked=false;
 function pagar(){
     alert("Gracias por su compra")
+}
+
+function seleccion(){
+    if(document.getElementById("paypal").checked==true){
+    document.getElementById("eleccion").innerHTML="PAYPAL";
+}
+else if(document.getElementById("other").checked==true){
+    document.getElementById("eleccion").innerHTML="OTHERS";
+}
 }
