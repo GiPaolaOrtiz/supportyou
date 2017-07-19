bfef6c2dd5e298e7ceb77681b3d37bb8aa4209a8
Walter Fabre <walterfa@outlook.es>
2017-07-19 03:29:49 -0500

    updating pago

--- js/pago.js
+++ js/pago.js
@@ -1,18 +1,10 @@
-document.getElementById("paypal").checked=false;
-document.getElementById("other").checked=false;
 function pagar(){
     alert("Gracias por su compra")
 }
 
 function seleccion(){
-    if(document.getElementById("paypal").checked==true){
-    document.getElementById("eleccion").innerHTML="PAYPAL";
-    document.getElementById("other").checked=false;
-    document.getElementById("paypal").checked=false;
+   document.getElementById("eleccion").innerHTML="PAYPAL";
 }
-else if(document.getElementById("other").checked==true){
-    document.getElementById("eleccion").innerHTML="OTHERS";
-    document.getElementById("paypal").checked=false;
-    document.getElementById("other").checked=false;
-}
+function seleccion2(){
+   document.getElementById("eleccion").innerHTML="OTHERS";
 }
