23e855d1a7d9154463b849ff54fee1a4246c6282
JhnFbre <walterfa@outlook.es>
2017-08-28 00:02:59 -0500

    metodo pago

--- paginas/modelo/MetodoPago/MetodoPago.php
+++ paginas/modelo/MetodoPago/MetodoPago.php
@@ -22,7 +22,28 @@ class MetodoPago
      } 
      function getMetodo(){
        return $this->metodo;
-     } 
+     }
+  function showDemo($id) {
+    $row = self::$db->getRows("SELECT * FROM demo where id_demo= ?", array("{$id}"));
+    $ObjDemo= new Demo($row[0]{'id_demo'},$row[0]{'nombre'},$row[0]{'password'});
+    return $ObjDemo;        
+  }
+
+  function updateDemo($id, $nombre,$password){
+      $insertrow= self::$db->updateRow
+                  ("UPDATE public.demo SET nombre= ?, password = ? where id_demo = ?", 
+                  array( "{$nombre}", "{$password}",$id ));
+  }
+
+  function deleteDemo($id){
+    $insertrow= self::$db->deleteRow
+                  ("DELETE FROM public.demo where id_demo = ?", 
+                  array( $id ));
+  }
+  function createDemo($nombre,$password){
+    $insertrow= self::$db->insertRow
+                  ("INSERT INTO public.demo (nombre, password) VALUES (?, ?)", array("{$nombre}","{$password}"));
+  } 
 }
 
 ?> 
