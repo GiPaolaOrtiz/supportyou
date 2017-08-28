b09f948ea7e46e3caa13b74e0dc1ba1f2cc4d396
GiPaolaOrtiz <paogi_ortiz@hotmail.com>
2017-08-27 23:24:28 -0500

    Registro

---
+++ paginas/modelo/usuario/createusuario.php
@@ -0,0 +1,21 @@
+ <!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<body>
+
+
+<h2>Editar Objeto Demo</h2>
+
+ <form action="guardarDemo.php" method="post">
+  <div class="form-group">
+    <label for="email">Nombre:</label>
+    <input type="text" name="nombre" autofocus required />
+  </div>
+
+
+  <a href="index.php">Cancelar</a>
+  <input type="submit" value="Guardar Objeto Demo" />
+</form> 
+
+</body>
+</html> 
