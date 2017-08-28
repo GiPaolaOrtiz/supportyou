ee6bc9711a59003b00e67476433b3eb3a6585713
JhnFbre <walterfa@outlook.es>
2017-08-27 23:30:59 -0500

    Categoria

--- paginas/modelo/CategoriaProducto/CategoriaProductoCollector.php
+++ paginas/modelo/CategoriaProducto/CategoriaProductoCollector.php
@@ -17,5 +17,27 @@ class CategoriaProductoCollector extends Collector
     }
     return $arrayCategoriaProducto;        
   }
+  function showcategoriaproducto($id) {
+    $row = self::$db->getRows("SELECT * FROM categoriaproducto where id_categoriaproducto= ?", array("{$id}"));
+    $Obj= new categoriaproducto($row[0]{'idcategoriaproducto'},$row[0]{'nombre'};
+    return $Obj;        
+  }
+
+  function updatecategoriaproducto($id, $nombre,$password){
+      $insertrow= self::$db->updateRow
+                  ("UPDATE public.categoriaproducto SET nombre= ? where id_categoriaproducto = ?", 
+                  array( "{$nombre}", $id ));
+  }
+
+  function deletecategoriaproducto($id){
+    $insertrow= self::$db->deleteRow
+                  ("DELETE FROM public.categoriaproducto where idcategoriaproducto = ?", 
+                  array( $id ));
+  }
+  function createcategoriaproducto($nombre){
+    $insertrow= self::$db->insertRow
+                  ("INSERT INTO public.categoriaproducto (nombre) VALUES (?)", array("{$nombre}"));
+  }
+
 }
 ?>
