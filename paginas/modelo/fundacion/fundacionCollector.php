fdd3f66f14c5213d5b42df976f90b4a04b8d1d86
LissetteHuilca <lissette.huilca@gmail.com>
2017-09-13 14:55:46 -0500

    collector

--- paginas/modelo/fundacion/fundacionCollector.php
+++ paginas/modelo/fundacion/fundacionCollector.php
@@ -7,34 +7,36 @@ class fundacionCollector extends Collector
 {
   
   function showFundaciones() {
-    $rows = self::$db->getRows("SELECT * FROM fundacion ORDER BY id_fundacion ASC ");        
-    echo "Nuestras Fundaciones";
+    $rows = self::$db->getRows("SELECT * FROM fundacion ORDER BY idfundacion ASC ");  
     $arrayFundacion= array();        
     foreach ($rows as $c){
-      $aux = new claseRegistroFundacion($c{'id_fundacion'}, $c{'actividad'}, $c{'direccion'}, $c{'email'}, $c{'pass'}, $c{'ruc'}, $c{'pais_id'}, $c{'fundacioncategoria_id'}, $c{'ciudad_id'}, $c{'cuenta_id'}, $c{'telefono'}, $c{'nombre'});
+      $aux = new claseRegistroFundacion($c{'idfundacion'}, $c{'direccion'}, $c{'actividad'}, $c{'email'}, $c{'pass'}, $c{'ruc'}, $c{'idusuariofk'}, $c{'idpaisfk'}, $c{'idciudadfk'}, $c{'idcuentafk'}, $c{'fundacioncategoria'}, $c{'nombre'}, $c{'telefono'});
       array_push($arrayFundacion, $aux);
     }
     return $arrayFundacion;        
   }
 
   function showFundacion($id) {
-    $row = self::$db->getRows("SELECT actividad, direccion, telefono, nombre FROM fundacion WHERE id_fundacion = ? ", array("{$id}"));        
-    $ObjFundacion = new claseRegistroFundacion($row[0]{'direccion'},$row[0]{'telefono'},$row[0]{'nombre'});
+    $row = self::$db->getRows("SELECT direccion, actividad, nombre, telefono FROM fundacion WHERE idfundacion = ? ", array("{$id}"));        
+    $ObjFundacion = new claseRegistroFundacion($row[0]{'direccion'},$row[0]{'nombre'},$row[0]{'telefono'});
 
     return $ObjFundacion;        
   }
 
-  function updateFundacion($id,$actividad,$direccion,$email,$pass,$ruc,$pais_id,$fundacioncategoria_id,$ciudad_id,$cuenta_id,$telefono,$nombre) {
-    $insertrow = self::$db->updateRow("UPDATE public.fundacion SET actividad = ?, direccion = ?, email = ?, pass = ?, ruc = ?, pais_id = ?, fundacioncategoria_id = ?, ciudad_id = ?, cuenta_id = ?, telefono = ?, nombre = ? WHERE id_fundacion = ? ", array( "{$actividad}","{$direccion}","{$email}","{$pass}","{$ruc}","{$pais_id}","{$fundacioncategoria_id}","{$ciudad_id}","{$cuenta_id}","{$telefono}","{$nombre}",$id));   
-  }  
+  function updateFundacion($id,$direccion,$actividad,$email,$pass,$ruc,$idusuariofk,$idpaisfk,$idciudadfk,$idcuentafk,$fundacioncategoria,$nombre,$telefono) {
+    $insertrow = self::$db->updateRow
+                   ("UPDATE public.fundacion SET direccion = ?, actividad = ?, email = ?, pass = ?, ruc = ?, idusuariofk = ?, idpaisfk = ?, idciudadfk = ?, idcuentafk = ?, fundacioncategoria = ?, nombre = ?, telefono = ? WHERE idfundacion = ? ",
+                   array("{$direccion}","{$actividad}","{$email}","{$pass}","{$ruc}","{$idusuariofk}","{$idpaisfk}","{$idciudadfk}","{$idcuentafk}","{$fundacioncategoria}","{$nombre}","{$telefono}", $id ));   
+  }
+    
 
   function deleteFundacion($id) {
-    $deletetrow = self::$db->deleteRow("DELETE FROM public.fundacion WHERE id_fundacion = ? ", array( "{$id}"));   
+    $deletetrow = self::$db->deleteRow("DELETE FROM public.fundacion WHERE idfundacion = ? ", array( "{$id}"));   
   } 
 
 
-  function createFundacion($actividad,$direccion,$email,$pass,$ruc,$pais_id,$fundacioncategoria_id,$ciudad_id,$cuenta_id,$telefono,$nombre){
-    $insertrow = self::$db->insertRow("INSERT INTO public.fundacion (actividad,direccion,email,pass,ruc,pais_id,fundacioncategoria_id,ciudad_id,cuenta_id,telefono,nombre) VALUES (?,?,?,?,?,?,?,?,?,?,?)", array("{$actividad}","{$direccion}","{$email}","{$pass}","{$ruc}","{$pais_id}","{$fundacioncategoria_id}","{$ciudad_id}","{$cuenta_id}","{$telefono}","{$nombre}"));
+  function createFundacion($direccion,$actividad,$email,$pass,$ruc,$idusuariofk,$idpaisfk,$idciudadfk,$idcuentafk,$fundacioncategoria,$nombre,$telefono){
+    $insertrow = self::$db->insertRow("INSERT INTO public.fundacion (actividad,direccion,email,pass,ruc,pais_id,fundacioncategoria_id,ciudad_id,cuenta_id,telefono,nombre) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", array("{$actividad}","{$direccion}","{$email}","{$pass}","{$ruc}","{$pais_id}","{$fundacioncategoria_id}","{$ciudad_id}","{$cuenta_id}","{$telefono}","{$nombre}"));
   }
 
 }
