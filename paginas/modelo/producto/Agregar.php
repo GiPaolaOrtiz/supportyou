16215e31a1560012fc30f3549261439b743056a2
angiegs <abgs15@gmail.com>
2017-09-14 12:01:53 -0500

    Collector de productos editado

--- paginas/modelo/producto/Agregar.php
+++ paginas/modelo/producto/Agregar.php
@@ -1,27 +1,75 @@
-<?php
-	session_start();
-?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-<head>
-	<title>Actualizar</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <title>Administración</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="../../../img/LogoSupportYou.png">
+        <link href="../../../css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet"  href="../../../css/style.css">        
+        <link rel="stylesheet"  href="../../../css/estiloCatalogo.css">
+        <link rel="stylesheet"  href="../../../css/estiloCarro.css">
+        <link rel="stylesheet"  href="../../../css/estiloadmin.css">
+    </head>
+    <body>
+        <main>
+            <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header page-scroll">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span> 
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a href="index.php"><img id="estilo_logo" alt="logo" src="../../../img/LogoSupportYou.png"></a>
+                    </div>
 
-	<?php
-		$nombre=$_POST['nombre'];
-		$password=$_POST['password'];
-		echo "Edici&oacute;n en proeso .... <br>";
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <br>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                               <a class="page-scroll" href="../index.php">HOME</a>
+                            </li>
+                            <li>
+                               <a class="page-scroll" href="../../../index.html">SALIR</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>    
+        <?php 
+                echo '<h2 class="topspace text-center">Agregar un Producto</h2>';
+            ?>
+            <?php
+                $idcategoriaproducto=$_POST['idcategoriaproducto'];
+                $idfundacion=$_POST['idfundacion'];
+                $descripcion=$_POST['descripcion'];
+                $estado=$_POST['estado'];
+                $precio=$_POST['precio'];
+                $img=$_POST['img'];
+                $estadoventa=$_POST['estadoventa'];
+            
+            
+                include_once $_SERVER['DOCUMENT_ROOT'] . "/supportyou/paginas/modelo/producto/ProductoCollector.php";
+                $ProductoCollectorObj = new ProductoCollector();
+                $ProductoCollectorObj-> createProducto($idcategoriaproducto,$idfundacion,$descripcion,$estado,$precio, $img, $estadoventa);
 
-		include_once("DemoCollector.php");
-		$DemoCollectorObj= new DemoCollector();
-		$DemoCollectorObj-> createDemo($nombre,$password);
+                echo "<h3 class='topspace text-center'>El Producto tipo <b>" . $idcategoriaproducto . "</b> ha sido agregado</h3>";
+            ?>
+            <div>
+                <a href="view.php" class="btn btn-info center-block w70">Volver</a>
+            </div>
+        </main>
+         <script src="../../js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../../js/bootstrap.min.js"></script>
+        <footer id="footer1">
+        <p class="copyright text-muted small">Copyright &copy; SupportYou 2017. All Rights Reserved</p>
 
-		echo "Nombre: ". $nombre . "agregando con password: " . $password . "<br>";
-	?>
-	<div>
-		<a href="index.php">Volver al inicio</a>
-	</div>
-</body>
+    </footer>  
+    </body>
 </html>
--- paginas/modelo/producto/ProductoCollector.php
+++ paginas/modelo/producto/ProductoCollector.php
@@ -12,30 +12,36 @@ class productoCollector extends Collector
   
   function showProductos() {
     $rows = self::$db->getRows("SELECT * FROM producto ");        
-    echo "linea 1";
     $arrayProducto= array();        
     foreach ($rows as $c){
-      $aux = new Producto($c{'idproducto'},$c{'idcategoriaproducto'},$c{'idfundacion'},$c{'descripcion'},$c{'estado'},$c{'precio'},$c{'img'},$c{'idperfil'},$c{'estadoventa'});
+      $aux = new Producto($c{'idproducto'},$c{'idcategoriaproducto'},$c{'idfundacion'},$c{'descripcion'},$c{'estado'},$c{'precio'},$c{'img'},$c{'estadoventa'});
       array_push($arrayProducto, $aux);
     }
     return $arrayProducto;        
   }
  
 
-  function updateProductos($idproducto, $idcategoriaproducto,  $idfundacion, $descripcion, $estado, $precio, $img, $idperfil, $estadoventa){
+  function showProducto($id) {
+    $row = self::$db->getRows("SELECT * FROM producto where idproducto= ?", array("{$id}"));
+    $ObjDemo= new Producto($row[0]{'idproducto'},$row[0]{'idcategoriaproducto'},$row[0]{'idfundacion'},$row[0]{'descripcion'},$row[0]{'estado'},$row[0]{'precio'},$row[0]{'img'},$row[0]{'estadoventa'});
+    return $ObjDemo;        
+  }
+
+
+  function updateProductos( $id, $idcategoriaproducto,  $idfundacion, $descripcion, $estado, $precio, $img, $estadoventa){
       $insertrow= self::$db->updateRow
-                  ("UPDATE public.producto SET idcategoriaproducto= ?, idfundacion = ?, descripcion = ?, estado = ?, precio = ?, img = ?,  $estadoventa = ?, $idperfil = ? where idproducto = ?, ", 
-                  array( "{$idcategoriaproducto}", "{$idfundacion}", "{descripcion}","{estado}", "{precio}","{img}","{estadoventa}", "{idperfil}",$idproducto ));
+                  ("UPDATE public.producto SET idcategoriaproducto= ?, idfundacion = ?, descripcion = ?, estado = ?, precio = ?, img = ?,  estadoventa = ? where idproducto = ?, ", 
+                  array( "{$idcategoriaproducto}", "{$idfundacion}", "{$descripcion}","{$estado}", "{$precio}","{img}","{estadoventa}", "{idperfil}",$id ));
   }
 
   function deleteProducto($id){
     $insertrow= self::$db->deleteRow
                   ("DELETE FROM public.producto where idproducto = ?", 
-                  array( $idproducto ));
+                  array( $id ));
   }
-  function createProducto($idcategoriaproducto,$idfundacion, $descripcion, $estado, $precio, $img, $estadoventa){
+   function createUsuario($idcategoriaproducto, $idfundacion, $descripcion $estado, $precio,$img, $estadoventa){
     $insertrow= self::$db->insertRow
-                  ("INSERT INTO public.producto (idcategoriaproducto, idfundacion, descripcion, estado, precio, img, estadoventa) VALUES (?, ?)", array("{$idcategoriaproducto}","{idfundacion}","{descripcion}","{estado}","{precio}","{img}","{$estadoventa}"));
-  }
+                  ("INSERT INTO public.usuario (idcategoriaproducto,idfundacion, descripcion, estado, precio, img, estadoventa) VALUES (?,?,?,?,?,?,?)", array("{$idcategoriaproducto}","{$idfundacion}","{$descripcion}","{$estado}","{$precio}", "{$img}"));
+  }  
 }
 ?>
