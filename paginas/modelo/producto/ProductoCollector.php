3adbca7f440b09c8c6437f5d9cea1e39f807eb65
JhnFbre <walterfa@outlook.es>
2017-09-18 23:28:45 -0500

    duno

--- paginas/modelo/producto/ProductoCollector.php
+++ paginas/modelo/producto/ProductoCollector.php
@@ -15,8 +15,6 @@ class ProductoCollector extends Collector
     }
     return $arrayProducto;        
   }
- 
-
   function showProducto($id) {
     $row = self::$db->getRows("SELECT * FROM producto where idproducto= ?", array("{$id}"));
     $ObjProducto= new Producto($row[0]{'idproducto'},$row[0]{'description'},$row[0]{'estado'},$row[0]{'precio'},$row[0]{'img'},$row[0]{'estadoventa'},$row[0]{'idfundacionfk'},$row[0]{'idcategoriaproductofk'});
@@ -28,7 +26,7 @@ class ProductoCollector extends Collector
       $insertrow= self::$db->updateRow("UPDATE public.producto SET description = ?, estado = ?, precio = ?, img = ?,  estadoventa = ?, idfundacionfk = ?, idcategoriaproductofk = ? where idproducto = ? ", 
                   array( "{$description}", "{$estado}", "{$precio}","{$img}", "{$estadoventa}","{$idfundacion}","{$idcategoriaproducto}", $id));
   }
-
+     
   function deleteProducto($id){
     $insertrow= self::$db->deleteRow
                   ("DELETE FROM public.producto where idproducto = ?", 
