ca2fc1c72afb492aba2f706f79fe9b74d47aca03
angiegs <abgs15@gmail.com>
2017-09-19 00:23:57 -0500

    Cambios comentarios.

--- paginas/modelo/comentarios/Eliminarcomentario.php
+++ paginas/modelo/comentarios/Eliminarcomentario.php
@@ -1,7 +1,14 @@
 <?php
-  session_start();
-?>
+session_start();
 
+       if (!isset($_SESSION['user'])){
+            echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../index.php'>";
+        }else{
+            if(!$_SESSION['rol']==1){
+                echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../index.php'>";
+            }else{
+               
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -73,3 +80,7 @@ echo "Se ha eliminado id : ".$id. " </br>";
 
 </body>
 </html>
+<?php
+}
+        }
+?>
